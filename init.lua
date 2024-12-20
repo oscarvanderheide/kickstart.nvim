3c9de6188a700047678a91d4f6d5fa56ab0ece62
Oscar van der Heide <o.vanderheide@umcutrecht.nl>
2024-12-20 09:59:35 +0100

    transparnt background

--- init.lua
+++ init.lua
@@ -4,3 +4,4 @@ require 'config.autocommands'
 require 'config.lazy'
 -- Does julia-vim work without this?
 vim.cmd 'runtime macros/matchit.vim'
+vim.cmd 'highlight Normal ctermbg=none guibg=none' -- Transparent background
