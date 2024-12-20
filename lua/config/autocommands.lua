44d97e9969c683332b7780da420a2c7ee7af6b1a
Oscar van der Heide <o.vanderheide@umcutrecht.nl>
2024-12-20 10:16:12 +0100

    cleanup autocmds

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -16,15 +16,6 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
--- Open Telescope file finder when opening a directory
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    --   -- if vim.fn.argv(0) == '' then
-    --   --   require('telescope.builtin').find_files()
-    --   -- end
-  end,
-})
-
 -- Prevent automatically commenting lines
 vim.api.nvim_create_autocmd('FileType', {
   pattern = '*',
