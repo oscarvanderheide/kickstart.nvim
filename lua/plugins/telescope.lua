a6287f770f82ee2e39b90d6b77959438e424e927
Oscar van der Heide <o.vanderheide@umcutrecht.nl>
2025-01-15 11:01:01 +0100

    ignore .git files in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -41,6 +41,12 @@ return {
             find_command = { 'fd', '--type', 'f' }, -- use fd instead of the 'find' command, also hides hidden files by default
           },
         },
+        defaults = {
+          file_ignore_patterns = {
+            'node_modules',
+            '.git',
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -67,11 +73,11 @@ return {
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      -- vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Commandline as a floating window
-      -- vim.keymap.set('n', ':', '<cmd>Telescope cmdline<cr>', { desc = 'Cmdline' })
+      vim.keymap.set('n', ':', '<cmd>Telescope cmdline<cr>', { desc = 'Cmdline' })
       -- Slightly advanced example of overriding default behavior and theme
       -- vim.keymap.set('n', '<leader>/', function()
       --   -- You can pass additional configuration to Telescope to change the theme, layout, etc.
@@ -94,6 +100,7 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
       -- Shortcut for smart open
       vim.keymap.set('n', '<leader><leader>', function()
         require('telescope').extensions.smart_open.smart_open { cwd_only = true }
